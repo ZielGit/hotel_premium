f0aa537fc5c66d9beadf4b63809f6dcc5cd341f9
Frans <frans_421_12@hotmail.com>
2024-07-23 20:47:23 -0500

    Proyecto Base

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hotel Premium
+
+Sistema de gestión hotelera
+
+## Requisitos
+
+-   **SERVER**: Apache 2 o NGINX.
+-   **PHP**: 5.6
+
+## Credenciales
+
+```
+usuario: admin
+password: admin2018
+```
---
+++ core/controller/Database.php
@@ -0,0 +1,41 @@
+<?php
+
+class Database
+{
+	public static $db;
+	public static $con;
+	public $user;
+	public $pass;
+	public $host;
+	public $ddbb;
+	function Database()
+	{
+		$this->user = "root";
+		$this->pass = "";
+		$this->host = "localhost";
+		$this->ddbb = "hotel_s1";
+	}
+
+	function connect()
+	{
+		$con = new mysqli($this->host, $this->user, $this->pass, $this->ddbb);
+		$con->query("set sql_mode=''");
+		return $con;
+	}
+
+	function connect1()
+	{
+		$db = new PDO("mysql:host=$this->host;", $this->user, $this->pass);
+		$db->exec("use `$this->ddbb`");
+		return $db;
+	}
+
+	public static function getCon()
+	{
+		if (self::$con == null && self::$db == null) {
+			self::$db = new Database();
+			self::$con = self::$db->connect();
+		}
+		return self::$con;
+	}
+}
